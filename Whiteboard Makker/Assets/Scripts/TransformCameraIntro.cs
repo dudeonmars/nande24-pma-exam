3d3153cb2a3b38cc6b6ca240b3b4c98e4154d8c2
dudeonmars <nande24@student.aau.dk>
2025-05-14 20:09:07 +0200

    Exam updates commit
    Updated the CamTransform to zoom before beginning sequence. Added animations for the story subjects. Cleaned out redundancies.

--- Whiteboard Makker/Assets/Scripts/TransformCameraIntro.cs
+++ Whiteboard Makker/Assets/Scripts/TransformCameraIntro.cs
@@ -3,34 +3,53 @@ using UnityEngine;
 
 public class TransformCameraIntro : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    void Start() {
        TransformCamera();
     }
 
-   void TransformCamera()
-    {
-        // Get the camera component
-        var cam = GetComponent<Camera>();
-
+   void TransformCamera() {
         StartCoroutine(ZoomSequensce());
     }
 
     private IEnumerator ZoomSequensce() {
+        var player = GameObject.FindGameObjectsWithTag("Player"); // NEW CODE AFTER GAMEJAM
+        var playerAnim = GameObject.FindGameObjectsWithTag("PlayerAnim"); // NEW CODE AFTER GAMEJAM
+        var princessAnim = GameObject.FindGameObjectsWithTag("PrincessAnim"); // NEW CODE AFTER GAMEJAM
+        var dragonAnim = GameObject.FindGameObjectsWithTag("DragonAnim"); // NEW CODE AFTER GAMEJAM
+        
         var cam = GetComponent<Camera>();
+
+        player[0].GetComponent<Movement>().enabled = false; // NEW CODE AFTER GAMEJAM
+        
+        playerAnim[0].SetActive(false); // NEW CODE AFTER GAMEJAM
+        princessAnim[0].SetActive(false); // NEW CODE AFTER GAMEJAM
+        dragonAnim[0].SetActive(false); // NEW CODE AFTER GAMEJAM
+        
         cam.transform.position = new Vector3(36.7f, -10+46.9f, -10);
         cam.orthographicSize = 46 ;
-        yield return ZoomIn(cam, 20f, 1f, 16f, new Vector3(55+36.7f, 46.9f, -10));
+        
+        yield return ZoomIn(cam, 36f, 14f, 2f, new Vector3(36.7f, -10+46.9f, -10)); // NEW CODE AFTER GAMEJAM
+        
+        yield return ZoomIn(cam, 20f, 1f, 0f, new Vector3(55+36.7f, 46.9f, -10)); //CHANGED CODE AFTER GAMEJAM
+        
+        princessAnim[0].SetActive(true); // NEW CODE AFTER GAMEJAM
+        
         yield return ZoomIn(cam, 10f, 1f, 5f, new Vector3(25+36.7f, -12+46.9f, -10));
+        
         yield return ZoomIn(cam, 12f, 2f, 3f, new Vector3(-2+36.7f, -5+46.9f, -10));
+        
+        playerAnim[0].SetActive(true); // NEW CODE AFTER GAMEJAM
+        
         yield return ZoomIn(cam, 15, 1f, 6.5f, new Vector3(35+36.7f, 5+46.9f, -10));
+
+        dragonAnim[0].SetActive(true); // NEW CODE AFTER GAMEJAM
+        
         yield return ZoomIn(cam, 12f, 3f, 8f, new Vector3(-2+36.7f, -5+46.9f, -10));
+        
+        player[0].GetComponent<Movement>().enabled = true; // NEW CODE AFTER GAMEJAM
     }
 
-    private IEnumerator ZoomIn(Camera cam, float targetSize, float duration, float delay, Vector3 targetPosition)
-    {
-        // Wait for the specified delay before starting the zoom
+    private IEnumerator ZoomIn(Camera cam, float targetSize, float duration, float delay, Vector3 targetPosition) {
         yield return new WaitForSeconds(delay);
 
         float startSize = cam.orthographicSize;
